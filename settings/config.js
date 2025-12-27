d9e5f50d91925c1683a33c2737b1f2d8f4e59f11
rahulvishawakarma70 <rahulvishawakarma7017@gmail.com>
2025-12-27 22:02:51 +0530

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,11 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['62']
+
+//================================//
+// Global Mess
+//================================//
+global.mess = {
+  owner: '❌ This command is only for the *owner*!',
+  premium: '💎 This feature is only available for *premium users*!',
+  group: '👥 This feature can only be used in *group chats*!',
+}
